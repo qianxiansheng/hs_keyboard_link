3e9048082b3d85654f4825bf3e6716c447ec9cdb
Chuxuan Huang <1372544416@qq.com>
2023-12-01 15:15:59 +0800

    使用 u8替换函数转换

--- ResearchPlayground/settings/settings.cpp
+++ ResearchPlayground/settings/settings.cpp
@@ -18,23 +18,23 @@ void ShowSettingsWindow(bool* p_open)
 	char softVersion[] = "--";
 	char hardVersion[] = "--";
 
-	ImGui::SeparatorText(utils::gbk_to_utf8("设备信息").c_str());
-	ImGui::Text(utils::gbk_to_utf8("产品型号：%s").c_str(), xinghao);
-	ImGui::Text(utils::gbk_to_utf8("软件版本：%s").c_str(), softVersion);
-	ImGui::Text(utils::gbk_to_utf8("硬件版本：%s").c_str(), hardVersion);
+	ImGui::SeparatorText(u8"设备信息");
+	ImGui::Text(u8"产品型号：%s", xinghao);
+	ImGui::Text(u8"软件版本：%s", softVersion);
+	ImGui::Text(u8"硬件版本：%s", hardVersion);
 
 
-	ImGui::SeparatorText(utils::gbk_to_utf8("语言").c_str());
+	ImGui::SeparatorText(u8"语言");
 
 	static int item_current_2 = 0;
-	ImGui::Combo("##language", &item_current_2, "Chinese\0English\0\0");
+	ImGui::Combo("##LANGUAGE", &item_current_2, u8"简体中文\0English\0\0");
 
 	static int idle_time = 0;
-	ImGui::SeparatorText(utils::gbk_to_utf8("休眠").c_str());
+	ImGui::SeparatorText(u8"休眠");
 	ImGui::SliderInt("##IDLE_TIME", &idle_time, 1, 20);
 
-	ImGui::SeparatorText(utils::gbk_to_utf8("恢复出厂设置").c_str());
-	ImGui::Button(utils::gbk_to_utf8("恢复出厂设置").c_str());
+	ImGui::SeparatorText(u8"恢复出厂设置");
+	ImGui::Button(u8"恢复出厂设置");
 
 	ImGui::End();
 }
